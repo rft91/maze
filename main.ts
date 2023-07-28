17b2ba5295a6eb890ef17b0b3a37312edf8c2547
rft91 <101027948+rft91@users.noreply.github.com>
2023-07-28 20:27:21 +0800

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,38 @@
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestOpen, function (sprite, location) {
-    score += 1
+    let score = 0
+    info.changeScoreBy(1)
+    if (score == 1) {
+        tiles.setTilemap(tilemap`level_3`)
+    } else if (score == 2) {
+    	
+    } else if (score == 3) {
+    	
+    } else if (score == 4) {
+    	
+    } else if (score == 5) {
+    	
+    } else if (score == 6) {
+    	
+    } else if (score == 7) {
+    	
+    } else if (score == 8) {
+    	
+    } else if (score == 9) {
+    	
+    } else if (score == 10) {
+    	
+    } else if (score == 11) {
+    	
+    } else {
+    	
+    }
+    tiles.placeOnRandomTile(mySprite, sprites.dungeon.chestClosed)
+    scene.cameraFollowSprite(mySprite)
+    info.startCountdown(200)
 })
-let score = 0
-let mySprite = sprites.create(img`
+let mySprite: Sprite = null
+info.setScore(0)
+mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . 2 2 2 2 2 2 . . . . 
@@ -24,13 +54,4 @@ controller.moveSprite(mySprite, 100, 100)
 tiles.setTilemap(tilemap`level_0`)
 tiles.placeOnRandomTile(mySprite, sprites.dungeon.chestClosed)
 scene.cameraFollowSprite(mySprite)
-info.startCountdown(10)
-forever(function () {
-    while (score == 1) {
-        info.startCountdown(10)
-        tiles.setTilemap(tilemap`level1`)
-        tiles.placeOnRandomTile(mySprite, sprites.dungeon.chestClosed)
-        scene.cameraFollowSprite(mySprite)
-        info.startCountdown(10)
-    }
-})
+info.startCountdown(200)
